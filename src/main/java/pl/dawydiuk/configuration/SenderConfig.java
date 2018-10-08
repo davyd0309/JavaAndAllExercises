e509fff2fd909a355e671add2b8beb2db65f96d4
Konrad Dawydiuk <Konrad.Dawydiuk.ctr@sabre.com>
2018-10-08 14:40:25 +0200

    master- commit 4

--- src/main/java/pl/dawydiuk/configuration/SenderConfig.java
+++ src/main/java/pl/dawydiuk/configuration/SenderConfig.java
@@ -42,4 +42,9 @@ public class SenderConfig {
         return new KafkaTemplate<>(producerFactory());
     }
 
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemsssplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
 }
